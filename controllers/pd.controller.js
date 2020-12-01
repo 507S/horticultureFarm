e6b970c4513b57551eb44b3d45af3fe5d5baba49
zahid0033 <zahidhossain0033@gmail.com>
2020-12-01 12:49:27 +0600

    center info err

--- controllers/pd.controller.js
+++ controllers/pd.controller.js
@@ -392,7 +392,7 @@ module.exports.center=async(req,res)=>{
         res.render('pd/centerinfo/center', { title: 'সেন্টারের যোগাযোগ তথ্য',success:'', records: data });
     })
     .catch(err => {
-        console.log("outside");
+        console.log(err);
         res.render('pd/centerinfo/center', { title: 'সেন্টারের যোগাযোগ তথ্য',success:'', records: err });
     })
      
